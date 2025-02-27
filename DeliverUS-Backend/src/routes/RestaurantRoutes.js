342f6912692b6c0b2930e9223c6c12088366eca0
Javier Luque <javluqrui@alum.us.es>
2025-02-27 12:46:32 +0100

    Lab2-Hecho

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -2,6 +2,20 @@ import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
   // TODO: Include routes for restaurant described in the lab session README
+  app.route('/restaurants')
+    .get(
+      RestaurantController.index)
+    .post(
+      // TODO: Add needed middlewares
+      RestaurantController.create)
 
+  app.route('/restaurants/:restaurantId')
+    .get(RestaurantController.show)
+    .put(
+      // TODO: Add needed middlewares
+      RestaurantController.update)
+    .delete(
+      // TODO: Add needed middlewares
+      RestaurantController.destroy)
 }
 export default loadFileRoutes
